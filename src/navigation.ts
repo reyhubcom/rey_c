a4f1b17a8767f3ffa29998450a7b14bf8d78d42e
reyhubcom <94319232+reyhubcom@users.noreply.github.com>
2025-09-27 09:26:00 -0400

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -7,7 +7,7 @@ export const headerData = {
       links: [
         {
           text: 'What is Staking?',
-          href: getPermalink('/homes/startup'),
+          href: getPermalink('/homes/staking'),
         },
         {
           text: 'Bitcoin Staking',
